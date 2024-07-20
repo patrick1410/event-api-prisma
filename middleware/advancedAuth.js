a0f59c70b0450ab3e960a36a824482ab39850e2c
Patrick <patrick_btc_96@hotmail.com>
2024-07-20 15:58:02 +0200

    added prisma!

--- middleware/advancedAuth.js
+++
@@ -1,6 +0,0 @@
-import { auth } from "express-oauth2-jwt-bearer";
-
-export const checkJwt = auth({
-  audience: "https://event-api-prisma",
-  issuerBaseURL: `https://dev-ezil5aozoabyhhcn.eu.auth0.com/`,
-});

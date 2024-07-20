15e7aa86b30a84531736dbd8342c57b3aa1ec99e
Patrick <patrick_btc_96@hotmail.com>
2024-07-20 17:21:36 +0200

    updated the seed script and the database!

---
+++ prisma/seed.js
@@ -0,0 +1,60 @@
+import { PrismaClient } from "@prisma/client";
+import categoryData from "../data/categories.json" assert { type: "json" };
+import eventData from "../data/events.json" assert { type: "json" };
+import userData from "../data/users.json" assert { type: "json" };
+
+const prisma = new PrismaClient({ log: ["query", "info", "warn", "error"] });
+
+async function main() {
+  const { categories } = categoryData;
+  const { events } = eventData;
+  const { users } = userData;
+
+  for (const category of categories) {
+    await prisma.category.upsert({
+      where: { id: category.id },
+      update: {},
+      create: category,
+    });
+  }
+
+  for (const user of users) {
+    await prisma.user.upsert({
+      where: { id: user.id },
+      update: {},
+      create: user,
+    });
+  }
+
+  for (const event of events) {
+    await prisma.event.upsert({
+      where: { id: event.id },
+      update: {},
+      create: {
+        id: event.id,
+        title: event.title,
+        description: event.description,
+        startTime: event.startTime,
+        endTime: event.endTime,
+        location: event.location,
+        image: event.image,
+        categories: {
+          connect: event.categoryIds.map((id) => ({ id })),
+        },
+        createdBy: {
+          connect: { id: event.createdBy },
+        },
+      },
+    });
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
